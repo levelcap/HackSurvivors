38fe420ac671d01c44e2f8a4a62bc0795094f844
Ben LaVerriere <ben.laverriere@bookbub.com>
2024-03-30 18:27:06 -0400

    Add flavortext to more upgrades

--- Survivors Clone/Utility/upgrade_db.gd
+++ Survivors Clone/Utility/upgrade_db.gd
@@ -166,32 +166,32 @@ const UPGRADES = {
 	},
 	"armor1": {
 		"icon": ICON_PATH + "helmet_1.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By 1 point",
+		"displayname": "Apron",
+		"details": "Reduces damage by 1 point",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
 	"armor2": {
 		"icon": ICON_PATH + "helmet_1.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By an additional 1 point",
+		"displayname": "Apron",
+		"details": "Reduces damage by an additional 1 point",
 		"level": "Level: 2",
 		"prerequisite": ["armor1"],
 		"type": "upgrade"
 	},
 	"armor3": {
 		"icon": ICON_PATH + "helmet_1.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By an additional 1 point",
+		"displayname": "Apron",
+		"details": "Reduces damage by an additional 1 point",
 		"level": "Level: 3",
 		"prerequisite": ["armor2"],
 		"type": "upgrade"
 	},
 	"armor4": {
 		"icon": ICON_PATH + "helmet_1.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By an additional 1 point",
+		"displayname": "Apron",
+		"details": "Reduces damage by an additional 1 point",
 		"level": "Level: 4",
 		"prerequisite": ["armor3"],
 		"type": "upgrade"
@@ -199,7 +199,7 @@ const UPGRADES = {
 	"speed1": {
 		"icon": ICON_PATH + "boots_4_green.png",
 		"displayname": "Speed",
-		"details": "Movement Speed Increased by 50% of base speed",
+		"details": "Movement speed increased by 50% of base speed",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "upgrade"
@@ -230,87 +230,87 @@ const UPGRADES = {
 	},
 	"tome1": {
 		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
+		"displayname": "Costco Membership",
+		"details": "Increases the impact of your tools by 10% of their base size",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
 	"tome2": {
 		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
+		"displayname": "Costco Membership",
+		"details": "Increases the impact of your tools by an additional 10% of their base size",
 		"level": "Level: 2",
 		"prerequisite": ["tome1"],
 		"type": "upgrade"
 	},
 	"tome3": {
 		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
+		"displayname": "Costco Membership",
+		"details": "Increases the impact of your tools by an additional 10% of their base size",
 		"level": "Level: 3",
 		"prerequisite": ["tome2"],
 		"type": "upgrade"
 	},
 	"tome4": {
 		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
+		"displayname": "Costco Membership",
+		"details": "Increases the impact of your tools by an additional 10% of their base size",
 		"level": "Level: 4",
 		"prerequisite": ["tome3"],
 		"type": "upgrade"
 	},
 	"scroll1": {
 		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
+		"displayname": "Caffeine Boost",
+		"details": "Increase attack speed by 5%",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
 	"scroll2": {
 		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
+		"displayname": "Caffeine Boost",
+		"details": "Increase attack speed by 5%",
 		"level": "Level: 2",
 		"prerequisite": ["scroll1"],
 		"type": "upgrade"
 	},
 	"scroll3": {
 		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
+		"displayname": "Caffeine Boost",
+		"details": "Increase attack speed by 5%",
 		"level": "Level: 3",
 		"prerequisite": ["scroll2"],
 		"type": "upgrade"
 	},
 	"scroll4": {
 		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
+		"displayname": "Caffeine Boost",
+		"details": "Increase attack speed by 5%",
 		"level": "Level: 4",
 		"prerequisite": ["scroll3"],
 		"type": "upgrade"
 	},
 	"ring1": {
 		"icon": ICON_PATH + "urand_mage.png",
-		"displayname": "Ring",
-		"details": "Your spells now spawn 1 more additional attack",
+		"displayname": "Ambidexterity",
+		"details": "Each of your tools now spawns one additional attack",
 		"level": "Level: 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
 	"ring2": {
 		"icon": ICON_PATH + "urand_mage.png",
-		"displayname": "Ring",
-		"details": "Your spells now spawn an additional attack",
+		"displayname": "Tripledexterity",
+		"details": "Yes, a third hand.",
 		"level": "Level: 2",
 		"prerequisite": ["ring1"],
 		"type": "upgrade"
 	},
 	"food": {
-		"icon": ICON_PATH + "chunk.png",
-		"displayname": "Food",
+		"icon": ICON_PATH + "Firstaidkit_icon.png",
+		"displayname": "First Aid Kit",
 		"details": "Heals you for 20 health",
 		"level": "N/A",
 		"prerequisite": [],
